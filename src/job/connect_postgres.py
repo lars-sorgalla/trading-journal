22c696ffbc9078ffb7422c0d19b0fa3ee38c7917
Lars Sorgalla <lars.sorgalla@gmail.com>
2022-05-25 15:40:10 +0200

    fix: align current working tree state to dev branch
    For anything fancier such as git merge with only keeping a few
    changes I am still too dumb

--- src/job/connect_postgres.py
+++ src/job/connect_postgres.py
@@ -29,8 +29,8 @@ def _connect() -> pyodbc.Connection:
 def load_pg_view_to_csv() -> None:
     connection = _connect()
     cursor = connection.cursor()
-    rows = cursor.execute("select * from ods.trading_journal_view order by 1")
-    with open("data-out/trading_journal_view.csv", mode="w", newline="") as f:
+    rows = cursor.execute("select * from ods.v_trading_journal order by 1")
+    with open("data-out/v_trading_journal_view", mode="w", newline="") as f:
         writer = csv.writer(f, delimiter=";")
         writer.writerow([elem[0] for elem in cursor.description])
         writer.writerows(rows)
