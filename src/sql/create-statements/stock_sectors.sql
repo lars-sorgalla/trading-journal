05a2ba7a67bcc402f2ae9df621222b4100a06852
Lars Sorgalla <lars.sorgalla@gmail.com>
2022-06-08 09:08:56 +0200

    feat: new table "stock_sectors" to analyze by sector

--- src/sql/create-statements/stock_sectors.sql
+++ src/sql/create-statements/stock_sectors.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS stock_sectors (
     stock_id SERIAL PRIMARY KEY
-    , ticker TEXT UNIQUE
+    , ticker TEXT UNIQUE NOT NULL
     , sector TEXT NOT NULL
     , industry TEXT NOT NULL
 );
+
+DROP TABLE stock_sectors
